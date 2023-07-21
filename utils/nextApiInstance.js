60edf47187819e433cb29549a18403c2de201a4d
remido <rakshayaroslav37@gmail.com>
2023-07-21 10:14:10 +0300

    com

--- utils/nextApiInstance.js
+++ utils/nextApiInstance.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const nextApiInstance = axios.create({
+    baseURL: 'https://trades-frontend.vercel.app',
     headers: {
         'Content-Type': 'application/json',
     }
